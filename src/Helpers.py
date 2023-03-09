443477d55bca9342bfbfe108e810c9748556f527
= <=>
2023-03-09 13:30:06 -0600

    adds sort validation function

--- src/Helpers.py
+++ src/Helpers.py
@@ -14,3 +14,10 @@ def random_fill(n):
             int_to_add = random.randint(1, n + 1)
         ints.append(int_to_add)
     return ints
+
+def is_sorted(in_array):
+    sorted = True
+    for i in range(0, len(in_array) - 1):
+        if in_array[i + 1] < in_array[i]:
+            sorted = False
+    return sorted
